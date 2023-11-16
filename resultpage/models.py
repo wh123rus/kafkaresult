40613d71a2b8102f49d8bd540f0a52183a6f9540
wh123rus <wh123rus@naver.com>
2023-11-16 09:00:57 +0000

    1

---
+++ resultpage/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import os
+
+# Create your models here.
+
+class Menu(models.Model):
+    pod_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    item = models.CharField(max_length=255)
+    number = models.IntegerField()
+    uuid = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.uuid  # 모델을 출력할 때 보여질 필드 선택
+    class Meta:
+        # 기존 테이블 이름 지정
+        db_table = os.getenv('DB_TABLE', 'testmenu')
---
+++ resultpage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Menu
+from django.core.paginator import Paginator  
+
+# Create your views here.
+def index(request):
+    page = request.GET.get('page', '1')  # 페이지
+
+    kafka_data = Menu.objects.all()
+
+    paginator = Paginator(kafka_data, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    
+    context = {'kafka_db_data': kafka_data}
+
+    return render(request, 'index.html', context)
