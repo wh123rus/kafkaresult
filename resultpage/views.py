1652fe8b3d65036e206d56f084cf56c54257ce04
wh123rus <wh123rus@naver.com>
2023-11-17 06:45:37 +0000

    Change html table, Add filtering

--- resultpage/views.py
+++ resultpage/views.py
@@ -3,9 +3,17 @@ from .models import Menu
 
 # Create your views here.
 def index(request):
+    item_list = ['Nike', 'Adidas', 'Newbalance', 'Salomon', 'Jordan', 'Converse']
+    
+    if request.method == 'POST':
+        item_filter = request.POST.get('item_filter')
+        if item_filter:
+            kafka_data = Menu.objects.filter(item=item_filter)
+        else:
+            kafka_data = Menu.objects.all()
+    else:
+        kafka_data = Menu.objects.all()
 
-    kafka_data = Menu.objects.all()
-
-    context = {'kafka_db_data': kafka_data}
+    context = {'kafka_db_data': kafka_data, 'item_list': item_list}
 
     return render(request, 'result.html', context)
