6e5f4da38efe309d993aaedd60f0c444e6d21acf
wh123rus <wh123rus@naver.com>
2023-11-17 07:38:54 +0000

    Add Pagenaiter in admin

--- resultpage/views.py
+++ resultpage/views.py
@@ -1,19 +1,31 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Menu
 
-# Create your views here.
 def index(request):
     item_list = ['Nike', 'Adidas', 'Newbalance', 'Salomon', 'Jordan', 'Converse']
     
-    if request.method == 'POST':
-        item_filter = request.POST.get('item_filter')
-        if item_filter:
-            kafka_data = Menu.objects.filter(item=item_filter)
-        else:
-            kafka_data = Menu.objects.all()
+    # 현재 필터링 상태 확인
+    item_filter = request.POST.get('item_filter') or request.GET.get('item_filter', '')
+
+    if item_filter:
+        kafka_data = Menu.objects.filter(item=item_filter)
     else:
         kafka_data = Menu.objects.all()
 
-    context = {'kafka_db_data': kafka_data, 'item_list': item_list}
+    # 페이징 추가
+    paginator = Paginator(kafka_data, 20)  # 한 페이지에 10개의 항목을 보여줍니다.
+    page = request.GET.get('page')
+
+    try:
+        kafka_data = paginator.page(page)
+    except PageNotAnInteger:
+        # 페이지 번호가 정수가 아닌 경우, 첫 번째 페이지를 보여줍니다.
+        kafka_data = paginator.page(1)
+    except EmptyPage:
+        # 페이지가 비어 있는 경우, 마지막 페이지를 보여줍니다.
+        kafka_data = paginator.page(paginator.num_pages)
+
+    context = {'kafka_db_data': kafka_data, 'item_list': item_list, 'current_item_filter': item_filter}
 
     return render(request, 'result.html', context)
